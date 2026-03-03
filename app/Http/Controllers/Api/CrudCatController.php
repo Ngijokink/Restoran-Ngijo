697ea4e2f7af2afe95957cbfa1877d25395fc52f
yohnzz <ignasiusmario05@gmail.com>
2026-03-03 10:11:48 +0700

    fix: migration transaction

--- app/Http/Controllers/Api/CrudCatController.php
+++ app/Http/Controllers/Api/CrudCatController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\Api;
 use App\Interfaces\CrudCatInterface;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Requests\CrudCatRequest;
 
 
--- database/migrations/2026_03_03_012841_create_table_transaction.php
+++ database/migrations/2026_03_03_012841_create_table_transaction.php
@@ -6,32 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('transaksi', function (Blueprint $table) {
-<<<<<<< HEAD
-            $table->unsignedBigInteger('id_orders');
-            $table->foreign('id_orders')->references('id_order')->on('table_orders')->onDelete('cascade');
-=======
-            $table->id();
+            $table->id(); // primary key
+
             $table->unsignedBigInteger('order_id');
->>>>>>> 52eff5d (fix: Users Model)
+
             $table->decimal('total', 10, 2);
             $table->string('method');
-            $table->string('status')->default(  'pending');
+            $table->string('status')->default('pending');
             $table->timestamp('paid_at')->nullable();
+
             $table->timestamps();
 
-            $table->foreign('order_id')->references('id_order')->on('table_orders')->onDelete('cascade');
+            $table->foreign('order_id')
+                  ->references('id_order')
+                  ->on('table_orders')
+                  ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('transaksi');
