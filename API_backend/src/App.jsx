0283c4defdeb5cec1e019a79938e45c46f0f48dd
yohnzz <ignasiusmario05@gmail.com>
2026-03-04 19:25:18 +0700

    fix: Axois to Axios

--- API_backend/src/App.jsx
+++ API_backend/src/App.jsx
@@ -2,23 +2,15 @@ import { Routes, Route } from "react-router-dom"
 import Login from "./Pages/Login";
 import Dashbord from "./Pages/Dashboard";
 import ProtectedRoute from "./ProtectedRoute/Protected.Route";
-import axios from "./Service/Axois"
+import axios from "./Service/Axios"
 
-  function App() {
-
+ function App() {
   return (
-      <Routes>
-     <Route path="/" element={<Login />} />
-      <Route
-      path="/Dashboard"
-      element={
-        <ProtectedRoute>
-          <Dashbord />
-        </ProtectedRoute>
-      }
-      />
-          </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
 
 export default App
