99f82b31bfdf61b51520b2226c39e83780a2f588
yohnzz <ignasiusmario05@gmail.com>
2026-03-04 14:55:11 +0700

    fix: endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\OrderItemController;
+use App\Http\Controllers\Api\ReportController;
+use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CatController;
@@ -20,11 +24,16 @@ use App\Http\Controllers\Api\MenuController;
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/register/asjdkbaskjdnaskdjbasdkasndaskjdbansdaskdjbasndmad/admin',[AuthController::class,'registerAdmin']);
-Route::resource('/categories',CatController::class);
 
 Route::middleware(['auth:sanctum','role:admin'])->group(function (){
     // Routes for Categories
+    Route::resource('/categories',CatController::class);
     Route::resource('/menus',MenuController::class);
+    Route::resource('/orders',OrderController::class);
+    // Route::resource('/export/pdf',ReportController::class);
+    Route::resource('/report',ReportController::class);
+    Route::get('/report/export-pdf',ReportController::class, '');
+    Route::resource('/transaction',TransactionController::class);
     Route::put('/users/{id}/role', [AuthController::class, 'updateRole']);
     Route::get('/users', [AuthController::class, 'user']);
     Route::post('/logout',[AuthController::class,'logout']);
